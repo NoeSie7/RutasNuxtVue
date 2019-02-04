f746208c76a8585d56aec351935fd8d5706b519b
NoeSie7 <kukulumbuku@gmail.com>
2019-02-04 15:24:05 +0000

    Añadido tablero y cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,110 @@
 <template>
-  <section class="container">
-  <div class="cols">
-    <div class="col-s3">
-      <h1>Ejemplo de rutas Con Vuex</h1>
-      <h2>JOJO</h2>
+  <section>
+    <div class="d-flex justify-content-center m-3">
+    <h1>Tablero</h1>
     </div>
+    <div class="container">
+      <div class="row">
+        <!-- <div class="col-sm">
+          <h3>Ejemplo de rutas Con Vuex</h3>
+        </div>
+        <div class="col-sm">
+          <h2>JOJO</h2>
+        </div> -->
+
+         <!-- <b-row>
+           <b-col v-for="i in 8" :key="i">
+              <div v-for="j in 8" :key="`${i}${j}`">{{`${i}${j}`}}</div>
+           </b-col>
+        </b-row>
+
+        <div class="tablero ">
+          <div class="celda" v-for="i in 8*8" :key="i">{{i}}</div>
+        </div> -->
+
+         <table id="tablero">
+      <tr>
+        <td>&#9820;</td>
+        <td>&#9822;</td>
+        <td>&#9821;</td>
+        <td>&#9819;</td>
+        <td>&#9818;</td>
+        <td>&#9821;</td>
+        <td>&#9822;</td>
+        <td>&#9820;</td>
+      </tr>
+      <tr>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+        <td>&#9823;</td>
+      </tr>
+      <tr>
+        <td>&#9820;</td>
+        <td>&#9822;</td>
+        <td>&#9821;</td>
+        <td>&#9819;</td>
+        <td>&#9818;</td>
+        <td>&#9821;</td>
+        <td>&#9822;</td>
+        <td>&#9820;</td>
+      </tr>
+    </table>
+      </div>
   </div>
   </section>
 </template>
@@ -18,6 +118,18 @@ export default {
   components: {
     Logo
   },
+  data(){
+    return {
+      matriz: [['hola','adios'],['hola1', 'adios1']],
+      matriz1: new Array(2),
+    }
+  },
+  created() {
+    this.matriz1[0] = new Array(8);
+    this.matriz1[1] = new Array(8);
+
+  }
+
 }
 </script>
 
@@ -51,5 +163,61 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.tablero{
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.celda{
+  flex-basis: calc(100%/8);
+
+}
+/* .celda:nth-child(3n+1){
+  background-color: red;
+
+}
+.celda:nth-child(2n){
+  background-color: yellow;
+
+} */
+/* .celda:nth-child(even){
+  background-color: red;
+  grid-row: 1 / 2;
+  grid-column: 2/3;
+
+}
+.celda:nth-child(odd){
+  background-color: yellow;
+
+} */
+
+#tablero{
+  background: url(http://jesuscastillo.esy.es/DisenoInterfacesWeb/Tema4/imagenes/madera.jpeg);
+  box-shadow: 0 10px 10px -6px rgba(0, 0, 0, .6);
+  border-radius: 3px;
+  border-collapse: collapse;
+  width: 400px;
+  height: 400px;
+  margin: auto;
+}
+
+ td{
+  color: #222;
+  font-size: 36px;
+  text-shadow: 1px 1px 1px rgba(255, 255, 255, 0.6);;
+  width: 50px;
+  height: 50px;
+}
+
+#tablero tr:nth-child(odd) td:nth-child(even),
+#tablero tr:nth-child(even) td:nth-child(odd){
+  background: rgba(0, 0, 0, 0.5);
+}
+
+#tablero tr:nth-child(n+7) td{
+  color: #fff;
+  text-shadow: 1px 1px 1px rgba(0,0,0,0.6);
 }
 </style>
--- pages/link.vue
+++ pages/link.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="container">
-    <h1>link</h1>
-  </div>
+<section class="container">
+  <div class="row">
+    <div class="col-10">
+  <h1 class="text-center m-2">Apunte su jugada</h1>
+   <ul class="tarjeta ">
+    <li v-for="i in 12" :key="i">
+      <a href="#">
+        <h2>Title #{{i}}</h2>
+        <p>Text Content #{{i}}</p>
+      </a>
+    </li>
+   </ul>
+   </div>
+   </div>
+ </section>
 </template>
 <script>
 export default {
@@ -9,6 +21,93 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+template{
+  font-family:arial,sans-serif;
+  font-size:100%;
+  margin:1em;
+  background:#666;
+  color:#fff;
+}
 
+h2,p{
+  font-size:100%;
+  font-weight:normal;
+}
+.tarjeta,li{
+  list-style:none;
+}
+.tarjeta{
+  overflow:hidden;
+  padding:3em;
+}
+.tarjeta li a{
+  text-decoration:none;
+  color:#000;
+  background:#ffc;
+  display:block;
+  height:10em;
+  width:10em;
+  padding:1em;
+  -moz-box-shadow:5px 5px 7px rgba(33,33,33,1);
+  -webkit-box-shadow: 5px 5px 7px rgba(33,33,33,.7);
+  box-shadow: 5px 5px 7px rgba(33,33,33,.7);
+  -moz-transition:-moz-transform .15s linear;
+  -o-transition:-o-transform .15s linear;
+  -webkit-transition:-webkit-transform .15s linear;
+}
+.tarjeta li{
+  margin:1em;
+  float:left;
+}
+.tarjeta li h2{
+  font-size:100%;
+  font-weight:bold;
+  padding-bottom:10px;
+}
+.tarjeta li p{
+  font-family:"Reenie Beanie",arial,sans-serif;
+  font-size:100%;
+}
+.tarjeta li a{
+  -webkit-transform: rotate(-6deg);
+  -o-transform: rotate(-6deg);
+  -moz-transform:rotate(-6deg);
+}
+.tarjeta li:nth-child(even) a{
+  -o-transform:rotate(4deg);
+  -webkit-transform:rotate(4deg);
+  -moz-transform:rotate(4deg);
+  position:relative;
+  top:5px;
+  background:#cfc;
+}
+.tarjeta li:nth-child(3n) a{
+  -o-transform:rotate(-3deg);
+  -webkit-transform:rotate(-3deg);
+  -moz-transform:rotate(-3deg);
+  position:relative;
+  top:-5px;
+  background:#ccf;
+}
+.tarjeta li:nth-child(5n) a{
+  -o-transform:rotate(5deg);
+  -webkit-transform:rotate(5deg);
+  -moz-transform:rotate(5deg);
+  position:relative;
+  top:-10px;
+}
+.tarjeta li a:hover,ul li a:focus{
+  box-shadow:10px 10px 7px rgba(0,0,0,.7);
+  -moz-box-shadow:10px 10px 7px rgba(0,0,0,.7);
+  -webkit-box-shadow: 10px 10px 7px rgba(0,0,0,.7);
+  -webkit-transform: scale(1.25);
+  -moz-transform: scale(1.25);
+  -o-transform: scale(1.25);
+  position:relative;
+  z-index:5;
+}
+ol{text-align:center;}
+ol li{display:inline;padding-right:1em;}
+ol li a{color:#fff;}
 </style>
